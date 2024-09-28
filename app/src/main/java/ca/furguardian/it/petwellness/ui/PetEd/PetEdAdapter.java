15b16687904a08ca42d855dc39718c181a71eed4
N01585098 <idzafurallah@gmail.com>
2024-09-27 21:25:56 -0400

    Commit, PetEd created a recycler view

---
+++ app/src/main/java/ca/furguardian/it/petwellness/ui/PetEd/PetEdAdapter.java
@@ -0,0 +1,46 @@
+package ca.furguardian.it.petwellness.ui.PetEd;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import ca.furguardian.it.petwellness.R;
+
+public class PetEdAdapter extends RecyclerView.Adapter<PetEdAdapter.ViewHolder> {
+
+    private List<String> mData;
+
+    public PetEdAdapter(List<String> data) {
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_peted, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = mData.get(position);
+        holder.textView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_item);
+        }
+    }
+}
