3374a7083e2a86196a58a3588c2ea8e62678a954
JustinChipman8472 <n01598472@humber.ca>
2024-10-08 20:01:10 -0400

    Rough Copies of lanscape support made

--- FurGuardian/src/main/java/ca/furguardian/it/petwellness/MainActivity.java
+++ FurGuardian/src/main/java/ca/furguardian/it/petwellness/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import ca.furguardian.it.petwellness.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
