4c38f03d9041ea950d413ef8f11f679459657f50
Zane Aransevia <aranseviazane@gmail.com>
2024-11-19 12:20:44 -0500

    Added group names to all new files.

--- FurGuardian/src/main/java/ca/furguardian/it/petwellness/ui/records/MedicalRecord.java
+++ FurGuardian/src/main/java/ca/furguardian/it/petwellness/ui/records/MedicalRecord.java
@@ -1,5 +1,8 @@
 package ca.furguardian.it.petwellness.ui.records;
-
+//Justin Chipman - N01598472
+//Imran Zafurallah - N01585098
+//Zane Aransevia - N01351168
+//Tevadi Brookes - N01582563
 public class MedicalRecord {
     private String date;
     private String type;
--- FurGuardian/src/main/java/ca/furguardian/it/petwellness/ui/records/RecordsFragment.java
+++ FurGuardian/src/main/java/ca/furguardian/it/petwellness/ui/records/RecordsFragment.java
@@ -1,9 +1,8 @@
+package ca.furguardian.it.petwellness.ui.records;
 //Justin Chipman - N01598472
 //Imran Zafurallah - N01585098
 //Zane Aransevia - N01351168
 //Tevadi Brookes - N01582563
-package ca.furguardian.it.petwellness.ui.records;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
